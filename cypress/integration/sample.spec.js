ec1e9d9e3d87a3b6f0a2c7284947333c79f4cd2a
Artem Shoiko <shoiko.a.s@gmail.com>
2021-09-02 23:29:41 +0300

    Add tests

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,16 +1,22 @@
+import Home from '../selectors/home'
+
 describe('Test for Simple SPA', () => {
   beforeEach(()=> {
     cy.visit('/')
   })
   it('Home tab', () => {
     cy.get('.header a[href="#/"]').click()
-    cy.get('.content h2').should('have.text', 'HELLO')
+    cy.get(Home.contentHeader).should('have.text', 'HELLO')
     cy.location('href').should('eq', `${Cypress.config().baseUrl}/#/`)
   })
-  it.skip('Stuff tab', () => {
-    
+  it('Stuff tab', () => {
+    cy.get('.header a[href="#/stuff"]').click()
+    cy.get(Home.contentHeader).should('have.text', 'STUFF')
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/#/stuff`)
   })
-  it.skip('Contact tab', () => {
-    
+  it('Contact tab', () => {
+    cy.get('.header a[href="#/contact"]').click()
+    cy.get(Home.contentHeader).should('have.text', 'GOT QUESTIONS?')
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/#/contact`)
   })
 })
